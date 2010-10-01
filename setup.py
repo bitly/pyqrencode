4d6cb71fef98b0899ad4cb8f3c3d2b8d689b2b64
Matt Reiferson <mreiferson@gmail.com>
2010-10-01 12:00:56 -0400

    optparse hack breaks pip, removing

--- setup.py
+++ setup.py
@@ -1,29 +1,10 @@
-import sys
-
 from distutils.core import setup
 from distutils.extension import Extension
 
-from optparse import OptionParser
-
-
 version = '0.2'
-
-parser = OptionParser()
-parser.add_option("", "--regenerate", action='store_true', dest='cython',
-                  help='regenerate C source via Cython (requires Cython > 0.13)')
-(options, args) = parser.parse_args()
-
-# remove our options from argv so distutils doesn't barf
-for i, a in enumerate(sys.argv):
-    if a == '--regenerate':
-        sys.argv.pop(i)
 
 cmdclass = {}
 source_files = ['qrencode.c']
-if options.cython:
-    from Cython.Distutils import build_ext
-    cmdclass = { 'build_ext': build_ext }
-    source_files = ['qrencode.pyx']
 
 setup(
     name="pyqrencode",
